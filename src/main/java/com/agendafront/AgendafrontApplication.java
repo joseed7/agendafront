ca54423636647bc5e8d0a22ef4357042306aa465
joseed7 <Joseescobar1114111@correo.itm.edu.co>
2025-11-12 17:16:12 -0500

    inicio

---
+++ src/main/java/com/agendafront/AgendafrontApplication.java
@@ -0,0 +1,13 @@
+package com.agendafront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendafrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendafrontApplication.class, args);
+    }
+
+}
