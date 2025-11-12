a39ba2735c7172905f1d7f158be30a11426d3494
joseed7 <Joseescobar1114111@correo.itm.edu.co>
2025-11-12 18:31:24 -0500

    configuracion final

--- src/main/java/com/agendafront/AgendafrontApplication.java
+++ src/main/java/com/agendafront/AgendafrontApplication.java
@@ -1,13 +1,36 @@
 package com.agendafront;
 
+import com.agendafront.window.MainFrame;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.*;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AgendafrontApplication {
 
+    @Autowired
+    private MainFrame mainFrame;
+
     public static void main(String[] args) {
+        // Configurar para modo no headless (importante para Swing)
+        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(AgendafrontApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // Ejecutar en el Event Dispatch Thread de Swing
+            SwingUtilities.invokeLater(() -> {
+                mainFrame.initializeUI();
+                mainFrame.showFrame();
+            });
+        };
+    }
 }
